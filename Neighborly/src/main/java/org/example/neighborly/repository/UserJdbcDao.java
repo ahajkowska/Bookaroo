9c5084a99b2339da4257ee1c161afb3cc9d730cf
Amelia <ameliahajkowska@gmail.com>
2025-11-30 23:56:51 +0100

    imports corrected

--- Neighborly/src/main/java/org/example/neighborly/repository/UserJdbcDao.java
+++ Neighborly/src/main/java/org/example/neighborly/repository/UserJdbcDao.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java. sql.SQLException;
-import java. time.LocalDateTime;
 import java. util.List;
 import java. util.Map;
 import java.util.UUID;
--- Neighborly/src/main/java/org/example/neighborly/repository/UserRepository.java
+++ Neighborly/src/main/java/org/example/neighborly/repository/UserRepository.java
@@ -2,7 +2,7 @@ package org.example.neighborly.repository;
 
 import org.example.neighborly.entity.User;
 import org.springframework.data.domain.Page;
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
