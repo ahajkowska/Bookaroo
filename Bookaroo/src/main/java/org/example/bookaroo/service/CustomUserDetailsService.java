60267ef394f473599f515d5f066cbea5061f99ce
Amelia <ameliahajkowska@gmail.com>
2025-12-23 22:49:02 +0100

    update customer details used in views

--- Bookaroo/src/main/java/org/example/bookaroo/service/CustomUserDetailsService.java
+++ Bookaroo/src/main/java/org/example/bookaroo/service/CustomUserDetailsService.java
@@ -2,10 +2,16 @@ package org.example.bookaroo.service;
 
 import org.example.bookaroo.entity.User;
 import org.example.bookaroo.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -17,14 +23,31 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Użytkownik nieznaleziony"));
 
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.getEmail())
-                .password(user.getPassword())
-                .roles(user.getRole())
-                .build();
+        var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+
+        return new BookarooUserDetails(
+                user.getUsername(),
+                user.getPassword(),
+                authorities,
+                user.getId()
+        );
+    }
+
+    public static class BookarooUserDetails extends org.springframework.security.core.userdetails.User {
+
+        private final UUID id;
+
+        public BookarooUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities, UUID id) {
+            super(username, password, authorities);
+            this.id = id;
+        }
+
+        public UUID getId() {
+            return id;
+        }
     }
 }
